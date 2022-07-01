22073229fae3076cb9ee4906cdac254e0e7cf809
shehu halimah sadiya <shehuhalimah15@gmail.com>
2022-07-01 13:54:03 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - ouput all single digit number of base 10
+ * main - output all single digit number of base 10
  *
  * Return: 0 if successful
  */
@@ -11,7 +11,7 @@ int main(void)
 	char var1;
 
 	for (var = 0; var < 10; var++)
-		putchar((var % 10) = '0');
+		putchar((var % 10) + '0');
 
 	for (var1 = 'a'; var1 <= 'f'; var1++)
 		putchar(var1);
