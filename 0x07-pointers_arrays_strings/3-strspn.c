2e2086e970c4f599693f5a6c9a2034dce334018f
shehu halimah sadiya <shehuhalimah15@gmail.com>
2022-07-19 10:39:20 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -6,27 +6,27 @@
  * @accept: prefix
  *
  * Return: the number of bytes in the initial segment of s
- * which consist only of byets from accept
+ * which consist only of bytes from accept
  */
-unsignedint _strspn(char *s, char *accept)
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int length = 0;
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		unsigned int length = 0;
-		int i, j;
-
-		for (i = 0; s[i] != '\0'; i++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			for (j = 0; accept[j] != '\0'; j++)
+			if (s[i] == acept[j])
 			{
-				if (s[i] == acept[j])
-				{
-					length++;
-					break;
-				}
-			}
-
-			if  (accept[j] == '\0')
+				length++;
 				break;
+			}
 		}
 
-		return (length);
+		if  (accept[j] == '\0')
+			break;
 	}
+
+	return (length);
+}
